4f833731e4731cc06c3109a76227a80dc2729571
SC <github.com@genkisugi.net>
2013-12-19 21:41:47 -0800

    third app; TODO: get complex_annotation data

--- server.js
+++ server.js
@@ -205,7 +205,7 @@ var Sugiyama = function() {
 			     // 	 {'name': 'foo', 'value': 1}
 			     // ],
 			     'body_content':
-			     'Try GO term ID URLs like: <br /><a style="color: blue;" href="/term/GO:0022008">/term/GO:0022008</a> (jsPlumb)<br /><a style="color: blue;" href="/node/GO:0022008">/node/GO:0022008</a> (cytoscape.js)'
+			     'Try GO term ID URLs like: <br /><a style="color: blue;" href="/term/GO:0022008">/term/GO:0022008</a> (jsPlumb)<br /><a style="color: blue;" href="/node/GO:0022008">/node/GO:0022008</a> (cytoscape.js)<br /><a style="color: blue;" href="/demo/textbook_glycolysis-mouse">/demo/textbook_glycolysis-mouse</a> (jsPlumb app prototype)'
 			 };
 			 //console.log(tmpl.toString());
 			 //console.log(tmpl_args);
@@ -259,8 +259,8 @@ var Sugiyama = function() {
 
 	// The request functions I use are very similar.
 	// This is for jsPlumb.
-	function create_request_function_1(personality, doc_type,
-					   id_field, label_field, graph_field){
+	function create_request_function(switch_type, personality, doc_type,
+					 id_field, label_field, graph_field){
 
 	    return function(req, res) {
 
@@ -301,7 +301,7 @@ var Sugiyama = function() {
 				'js_variables': [
 					 {
 					     'name': 'global_switch',
-					     'value': '"jsPlumb"'
+					     'value': '"' + switch_type + '"'
 					 },
 					 {
 					     'name': 'global_id',
@@ -332,92 +332,30 @@ var Sugiyama = function() {
 	}
 	// Dynamic GOlr output.
 	self.app.get('/term/:query',
-		     create_request_function_1('ontology',
-					       'ontology_class',
-					       'annotation_class',
-					       'annotation_class_label',
-					       'topology_graph_json'));
+		     create_request_function('jsPlumb',
+					     'ontology',
+					     'ontology_class',
+					     'annotation_class',
+					     'annotation_class_label',
+					     'topology_graph_json'));
 
-	// The request functions I use are very similar.
-	// This is for cytoscape.js.
-	function create_request_function_2(personality, doc_type,
-					   id_field, label_field, graph_field){
-
-	    return function(req, res) {
-
-		//console.log(req.route);
-		//console.log(req.route.params['query']);
-		var query = req.route.params['query'] || '';
-		if( ! query || query == '' ){
-		    // Catch error here if no proper ID.
-		    res.setHeader('Content-Type', 'text/html');
-		    res.send('no identifier');
-		}else{
-		    
-		    // Try AmiGO 2 action.
-		    var go = new bbop.golr.manager.nodejs(server_loc, gconf);
-		    go.set_personality(personality); // profile in gconf
-		    go.add_query_filter('document_category', doc_type);
-		    go.set_id(query);
-
-		    // Define what we do when our GOlr (async) information
-		    // comes back within the scope of the response we
-		    // need to end.
-		    function golr_callback_action(gresp){
-
-			// There should be only one return doc.
-			var doc = gresp.get_doc(0);
-			if( ! doc ){
-			    res.setHeader('Content-Type', 'text/html');
-			    res.send('bad doc:' + query);
-			}else{
-			    var id = doc[id_field];
-			    var label = doc[label_field];
-			    var graph = doc[graph_field];
-			    
-			    // Assemble return doc.
-			    res.setHeader('Content-Type', 'text/html');
-			    var tmpl = self.cache_get('frame.tmpl').toString();
-			    var tmpl_args = {
-				'js_variables': [
-					 {
-					     'name': 'global_switch',
-					     'value': '"cytoscape"'
-					 },
-					 {
-					     'name': 'global_id',
-					     'value': '"' + id + '"'
-					 },
-					 {
-					     'name': 'global_label',
-					     'value': '"' + label + '"'
-					 },
-					 {
-					     'name': 'global_graph',
-					     'value': graph
-					 }
-				],
-				'body_content': label + ' (' + id + ')'
-			    };
-			    var ret = mustache.render(tmpl, tmpl_args);
-			    res.send(ret);
-			}
-		    }
-
-		    // Run the agent action.
-		    go.register('search', 'do', golr_callback_action);
-		    go.update('search');
-		    //console.log('update: search');
-		}
-	    };
-	}
 	// Dynamic GOlr output.
 	self.app.get('/node/:query',
-		     create_request_function_2('ontology',
-					       'ontology_class',
-					       'annotation_class',
-					       'annotation_class_label',
-					       'topology_graph_json'));
+		     create_request_function('cytoscape',
+					     'ontology',
+					     'ontology_class',
+					     'annotation_class',
+					     'annotation_class_label',
+					     'topology_graph_json'));
+
+	// Dynamic GOlr output.
+	self.app.get('/demo/:query',
+		     create_request_function('expPlumb',
+					     'ontology',
+					     'ontology_class',
+					     'annotation_class',
+					     'annotation_class_label',
+					     'topology_graph_json'));
     };
 
     // Initializes the sample application.
--- static/App.js
+++ static/App.js
@@ -1,3 +1,153 @@
+var expPlumbInit = function(){
+    
+    var logger = new bbop.logger('jsPlumbApp');
+    logger.DEBUG = true;
+    function ll(str){ logger.kvetch(str); }
+    
+    // Aliases
+    var each = bbop.core.each;
+    var is_defined = bbop.core.is_defined;
+
+    var container_id = '#' + 'main_jsp';
+    
+    ///
+    /// jsPlumb preamble.
+    ///
+
+    var instance = jsPlumb.getInstance(
+	{
+	    DragOptions: {ccursor: 'pointer', zIndex:2000 },
+	    PaintStyle: { strokeStyle:'#666' },
+	    EndpointStyles : [{ fillStyle:"#0d78bc" },
+			      { width:15, height:15, strokeStyle:'#666', fillStyle:"#333" }],
+            Endpoints : [ ["Dot", { radius:7 } ], "Rectangle" ],
+	    //Endpoint : "Rectangle",
+//	    Anchor : "AutoDefault",
+	    // Anchors : [
+	    // 	"TopCenter",
+	    // 	"TopCenter",
+	    // 	"TopLeft",
+	    // 	"TopRight",
+	    // 	"BottomCenter",
+	    // 	"BottomCenter",
+	    // 	"BottomLeft",
+	    // 	"BottomRight"
+	    // ],
+	    PaintStyle : {
+		strokeStyle:"#558822",
+		lineWidth: 2
+	    },
+	    Container: "main_jsp"
+        });
+
+    ///
+    /// Graphy stuff.
+    ///
+
+    var id = global_id;
+    var label = global_label;
+    var graph_json = global_graph;
+
+    // Load graph and extract a layout.
+    var g = new bbop.model.graph();
+    g.load_json(graph_json);
+    var r = new bbop.layout.sugiyama.render();
+    var layout = r.layout(g);
+
+    // Add the necessary elements to the display.
+    var h_spacer = 75;
+    var v_spacer = 75;
+    var box_width = 120;
+    var box_height = 100;
+    var vbox_width = 5;
+    var vbox_height = 5;
+    function _box_top(raw_y){
+	return ((box_height + v_spacer) * raw_y) + v_spacer;	
+    }
+    function _box_left(raw_x){
+	return ((box_width + h_spacer) * raw_x) + h_spacer;
+    }
+    function _vbox_top(raw_y){
+	return _box_top(raw_y) + (box_height / 2.0);
+    }
+    function _vbox_left(raw_x){
+	return _box_left(raw_x) + (box_width / 2.0);
+    }
+
+    var term2div = {};
+    each(layout['nodes'],
+	 function(litem, index){
+
+	     var id = litem['id'];
+	     var raw_x = litem['x'];
+	     var raw_y = litem['y'];
+	     var div_id = 'ddid' + index;
+	     term2div[id] = div_id;
+
+	     jQuery(container_id).append('<div class="window" style="top: ' + _box_top(raw_y) + 'px; left: ' + _box_left(raw_x) + 'px;" id="' + div_id + '">' + id + '</div>');
+	 });
+    each(layout['virtual_nodes'],
+	 function(litem, index){
+
+	     var id = litem['id'];
+	     var raw_x = litem['x'];
+	     var raw_y = litem['y'];
+	     var div_id = 'vdid' + index;
+	     term2div[id] = div_id;
+
+	     jQuery(container_id).append('<div class="waypoint" style="top: ' + _vbox_top(raw_y) + 'px; left: ' + _vbox_left(raw_x) + 'px;" id="' + div_id + '">' + '' + '</div>');
+	 });
+
+    // Now that they are physically extant, add JS stuff.
+    
+    // Make nodes draggable.
+    var foo = jsPlumb.getSelector(".window");
+    instance.draggable(foo);
+    var bar = jsPlumb.getSelector(".waypoint");
+    instance.draggable(bar);
+
+    instance.doWhileSuspended(
+	function(){
+	    
+	    // 
+	    //instance.makeSource(jsPlumb.getSelector(".window"),{isSource:true});
+	    //instance.makeSource(jsPlumb.getSelector(".waypoint"),{isSource:true});
+	    instance.makeTarget(jsPlumb.getSelector(".window"));
+	    instance.makeTarget(jsPlumb.getSelector(".waypoint"));
+
+	    // Now let's try to add edges/connections.
+	    each(layout['paths'],
+    		 function(path){
+		     var nodes = path['nodes'];
+		     var waypoints = path['waypoints']; // don't need right now?
+		     for(var ni = 0; ni < (nodes.length -1); ni++ ){
+			 var sub_id = nodes[ni];
+			 var obj_id = nodes[ni +1];
+			 
+			 var sub_div = term2div[sub_id];
+			 var obj_div = term2div[obj_id];
+			 
+			 instance.connect(
+			     {
+			 	 source: sub_div,
+			 	 target: obj_div,
+				 anchors:["Top", "Bottom"],
+				 //connector:"Straight",
+                                 connector: ["Bezier", { curviness: 25 } ]
+			     });
+		     }
+       		 });
+	});
+
+    // jQuery(container_id).scroll(
+    //     function(){
+    //         jsPlumb.repaintEverything();
+    //     }
+    // );
+
+    //ll('starting:' + bbop.core.dump(layout));
+};
+
 var cytoscapeInit = function(){
 
     var logger = new bbop.logger('cytoscapeApp');
@@ -346,12 +496,23 @@ jsPlumb.ready(function(){
 		      typeof(global_label) !== 'undefined' &&
 		      typeof(global_graph) !== 'undefined' ){
 		      if( global_switch == 'cytoscape' ){
+			  jQuery('#main_jsp').remove();
+			  jQuery('#main_exp').remove();
 			  cytoscapeInit();
+		      }else if( global_switch == 'jsPlumb' ){
+			  jQuery('#main_cy').remove();
+			  jQuery('#main_exp').remove();
+			  jsPlumbInit();
+		      }else if( global_switch == 'expPlumb' ){
+			  jQuery('#main_cy').remove();
+			  jQuery('#main_jsp').remove();
+			  expPlumbInit();
 		      }else{
-			  jsPlumbInit();			  
+			  throw new Error('unknown switch in App');
 		      }
 		  }else{
 		      jQuery('#main_cy').remove();
 		      jQuery('#main_jsp').remove();
+		      jQuery('#main_exp').remove();
 		  }
 	      });
