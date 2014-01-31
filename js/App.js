d26a92dd4c4685feb89d59ccba01164402123714
SC <github.com@genkisugi.net>
2014-01-30 17:34:54 -0800

    correct deletion stubs already in

--- js/App.js
+++ js/App.js
@@ -276,7 +276,8 @@ var MMEnvInit = function(in_model, in_server_base){
 		function(evt){
 		    evt.stopPropagation();
 
-		    alert('node deletion not yet supported on the server');
+		    //alert('node deletion not yet supported on the server');
+		    ll('node deletion not yet supported on the server');
 
 		    // Delete ind and related edges.
 		    _delete_iae_from_ui(tid);
@@ -829,7 +830,8 @@ var MMEnvInit = function(in_model, in_server_base){
     // Detach event.
     instance.bind("connectionDetached", function(info) {
 
-		      alert('edge deletion not yet supported on the server');
+		      //alert('edge deletion not yet supported on the server');
+		      ll('edge deletion not yet supported on the server');
 
 		      var cid = info.connection.id;
 		      ll('there was a connection detached: ' + cid);
