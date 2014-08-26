18bed538bda6fa9d90faf59ea4a0bd22fd9e6947
SC <github.com@genkisugi.net>
2014-08-26 15:02:15 -0700

    upstream bump

--- static/bbop.js
+++ static/bbop.js
@@ -1886,7 +1886,7 @@ bbop.version.revision = "2.2.3";
  *
  * Partial version for this library: release (date-like) information.
  */
-bbop.version.release = "20140820";
+bbop.version.release = "20140826";
 /*
  * Package: logger.js
  * 
