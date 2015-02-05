af02781c5f6c9afaf3c3e034c42251486fb9d187
Jeremy Nguyen Xuan <jnguyenxuan@lbl.gov>
2015-02-05 14:58:15 -0800

    widgetize the search autocomplete boxes

--- js/NoctuaBasic.js
+++ js/NoctuaBasic.js
@@ -126,20 +126,57 @@ var MMEnvBootstrappingInit = function(user_token){
         alert_wrapper.css('display', 'inherit');
     }
 
+    var gene_selectize = jQuery('#select_gene_product').zearch({
+        onChange: function(value) {
+            gp_ractive.set('gp', value);
+        },
+        required: true,
+        optionDisplay: function(item, escape) {
+            return '<div>' +
+                item.id + " (" + item.annotation_class_label_searchable + ")" +
+            '</div>';
+        },
+        itemDisplay: function(item, escape) {
+            return '<div>' +
+                item.id + " (" + item.annotation_class_label_searchable + ")" +
+            '</div>';
+        },
+        valueField: 'id',
+        searchField: ['id', 'annotation_class_label_searchable'],
+        queryData: function(query) {
+            return {'wt':'json', 'fl':'id,annotation_class_label_searchable', 'q':'id:' + query.replace(':', '\\:').toUpperCase() + '*' + ' OR ' +
+                    'annotation_class_label_searchable:' + '*' + query + '*'}
+        }
+    });
+
+    var phen = jQuery('#select_phenotype_product').zearch({
+        onChange: function(value) {
+            console.log(value);
+        },
+        optionDisplay: function(item, escape) {
+            return '<div>' +
+                item.id + " (" + item.annotation_class_label_searchable + ")" +
+            '</div>';
+        },
+        itemDisplay: function(item, escape) {
+            return '<div>' +
+                item.id + " (" + item.annotation_class_label_searchable + ")" +
+            '</div>';
+        },
+        valueField: 'id',
+        searchField: ['id', 'annotation_class_label_searchable'],
+        queryData: function(query) {
+            return {'wt':'json', 'fl':'id,annotation_class_label_searchable', 'q':'id:' + query.replace(':', '\\:').toUpperCase() + '*' + ' OR ' +
+                    'annotation_class_label_searchable:' + '*' + query + '*'}
+        }
+    });
+
     var gp = "";
     var gp_ractive = new Ractive({
       el: 'gp_placeholder',
-      template: '{{#if gp==""}}<div>Cannot be null!</div>{{/if}}',
+      //template: '{{#if gp==""}}<div>Cannot be null!</div>{{/if}}',
       model: gp
     });
-
-    var hits = 0;
-    var hits_ractive = new Ractive({
-        el: 'hits_placeholder',
-        template: '{{#if isSearching}}{{hits}} hits{{/if}}',
-        model: hits,
-        data: {isSearching: false}
-    })
 
     var qualifier_ractive = new Ractive({
       el: 'qualifier_placeholder',
@@ -150,64 +187,6 @@ var MMEnvBootstrappingInit = function(user_token){
     // initialize model
     var id = null // dirty
     manager.generate_model();
-
-    var gp_selectize = jQuery('#select_gene_product').selectize({
-    valueField: 'id',
-    labelField: 'id',
-    searchField: ['id', 'annotation_class_label_searchable'],
-    options: [],
-    create: false,
-    render: {
-        option: function(item, escape) {
-            return '<div>' +
-                item.id + " (" + item.annotation_class_label_searchable + ")" +
-            '</div>';
-        },
-        item: function(item, escape) {
-            return '<div>' +
-                item.id + " (" + item.annotation_class_label_searchable + ")" +
-            '</div>';
-        }
-    },
-    load: function(query, callback) {
-        if (!query.length) return callback();
-        jQuery.ajax({
-            url: gserv + 'select',
-            data: {'wt':'json', 'fl':'id,annotation_class_label_searchable', 'q':'id:' + query.replace(':', '\\:').toUpperCase() + '*' + ' OR ' + 'annotation_class_label_searchable:' + '*' + encodeURIComponent(query) + '*'},
-            dataType: 'jsonp',
-            jsonp: 'json.wrf',
-            error: function() {
-                callback();
-            },
-            success: function(res) {
-                hits_ractive.set('hits', res.response.numFound);
-                hits = res.response.numFound;
-                //TODO load underscore.js
-                //var uniq_only = _.uniq(res.response.docs) // remove potential duplicates
-                //callback(uniq_only);
-                callback(res.response.docs);
-            }
-        });
-    }, 
-    onChange: function(value) {
-        gp_ractive.set('gp', value);
-    },
-    onType: function(str) {        
-        clearCache(); // in order to reset the hits count
-    },
-    onFocus: function() {
-        hits_ractive.set('isSearching', true);
-    },
-    onBlur: function(){
-        hits_ractive.set('isSearching', false);
-    }
-    });
-
-    var clearCache = function () {
-        gp_selectize[0].selectize.clearCache("option");
-        gp_selectize[0].selectize.clearOptions();
-    };
-
 
 };
 
@@ -232,3 +211,105 @@ jQuery(document).ready(
 	    MMEnvBootstrappingInit(user_token);
 	}
     });
+
+
+
+
+// Depends on selectize
+jQuery.widget('jj.zearch', {
+    options: {
+        webserviceUrl: "http://localhost:8983/solr/select",
+        onChange: console.log,
+        required: false,
+        optionDisplay: null,
+        itemDisplay: null,
+        valueField: null,
+        searchField: null,
+        queryData: null
+    },
+
+    _create: function () {
+        // redefine the variables to avoid conflicts with selectize's scope
+        var widgetOnChange = this.options.onChange;
+        var widgetWebservicerUrl = this.options.webserviceUrl;
+        var widgetRequired = this.options.required;
+        var widgetOptionDisplay = this.options.optionDisplay;
+        var widgetItemDisplay = this.options.itemDisplay;
+        var widgetValueField = this.options.valueField;
+        var widgetSearchField = this.options.searchField;
+        var widgetQueryData = this.options.queryData;
+        var _currentValue = null;
+
+        // Right label for user feedback
+        var feedbackLabel = jQuery('<div style="width:130px;padding-left:15px;"></div>');
+        this.element.after(feedbackLabel);
+
+        var selectized = this.element.selectize({
+            valueField: widgetValueField,
+            searchField: widgetSearchField,
+            create: false,
+            render: {
+                option: widgetOptionDisplay,
+                item: widgetItemDisplay
+            },
+            load: function(query, callback) {
+                if (!query.length) return callback();
+                jQuery.ajax({
+                    url: widgetWebservicerUrl,
+                    data: widgetQueryData(query),
+                    dataType: 'jsonp',
+                    jsonp: 'json.wrf',
+                    error: function() {
+                        callback();
+                    },
+                    success: function(res) {
+                        _updateHits(res.response.numFound);
+                        //TODO load underscore.js
+                        //var uniq_only = _.uniq(res.response.docs) // remove potential duplicates
+                        //callback(uniq_only);
+                        callback(res.response.docs);
+                    }
+                });
+            }, 
+            onChange: function(value) {
+                widgetOnChange(value);
+                _checkSanity(value);
+                _currentValue = value;
+            },
+            onType: function(str) {        
+                _clearCache(); // in order to request the server at each typing
+            },
+            onBlur: function() {
+                _checkSanity(_currentValue);
+            }
+        });
+
+        var _checkSanity = function(value) {
+            if(widgetRequired && (value == null || value == "")) {
+                feedbackLabel.text('Cannot be empty!');
+            } else {
+                feedbackLabel.text('');
+            }
+        }
+
+        var _clearCache = function () {
+            selectized[0].selectize.clearCache("option");
+            selectized[0].selectize.clearOptions();
+        };
+
+        var _updateHits = function(h) {
+            feedbackLabel.text(h + ' hits');
+        };
+
+    },
+
+    _destroy: function () {
+        selectized[0].selectize.destroy();
+    },
+
+
+    _setOptions: function (options) {
+        this._super( options );
+        this.refresh();
+    }
+});
