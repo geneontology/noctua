7570430d7de31c2dfe868d94d847bddb5a82a101
SC <github.com@genkisugi.net>
2016-11-03 01:32:10 -0700

    finish wiring for #350; TODO: get around hack from bbop-manager-minerva#1 for real multi-groups

--- js/NoctuaEditor.js
+++ js/NoctuaEditor.js
@@ -1656,6 +1656,8 @@ var MMEnvInit = function(model_json, in_relations, in_token){
 	    var reqs = new minerva_requests.request_set(manager.user_token(),
 							ecore.get_id());
 	    reqs.add_individual(simple_ubernoodle_auto_val);
+	    //console.log(reqs.structure());
+	    //console.log(manager.use_groups());
 	    manager.request_with(reqs);
 
 	    // Wipe controls' state, internal and external.
@@ -1672,7 +1674,7 @@ var MMEnvInit = function(model_json, in_relations, in_token){
 
 	// The base settings for bioentity autocomplete.
 	var base_enb_auto_args = {
-    	    'label_template': '{{annotation_class_label}} ({{annotation_class}})',
+    	    'label_template':'{{annotation_class_label}} ({{annotation_class}})',
     	    'value_template': '{{annotation_class_label}}',
 	    'additional_results_class': 'bbop-mme-more-results-ul'
 	};
@@ -2634,10 +2636,16 @@ var MMEnvInit = function(model_json, in_relations, in_token){
 	    global_barista_location, manager.user_token(), 'user_name_info',
 	    function(new_group_id){
 		if( ! new_group_id ){
-		    alert('cancelling current gid');
+		    manager.use_groups(null);
+		    // console.log('removing groups from user "' +
+		    // 		manager.user_token() + '"');
 		}else{
-		    alert('use group: ' + new_group_id);
+		    manager.use_groups([new_group_id]);
+		    // console.log('adding group "'+ new_group_id +'" to user ' +
+		    // 		manager.user_token());
 		}
+		console.log('current groups "' + manager.use_groups() +
+			    '" for user ' + manager.user_token());
 	    });
     }
 };
